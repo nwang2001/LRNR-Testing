7c44f03649cd37b996d05f058e18bedea7a0b6c9
Nikki Wang <nikkiw011201@gmail.com>
2024-02-02 17:11:29 -0500

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './footer.css';
-
 
 const Footer = () => {
     return (
-        <footer className="bg-light text-black footer">
-            <div className="container-fluid">
-                <div className="row justify-content-evenly">
-                    <div className="col-md-6">
-                        <p className="text-center">quote</p>
-                    </div>
-                    <div className="col-md-6">
-                        <p className="text-center">Disclaimer: This is a disclaimer :D</p>
-                    </div>
+        <footer className="footer">
+            <div className="footer-left col-md-4 col-sm-6">
+                <h2>
+                    {" "}
+                    LRNR<span> logo</span>
+                </h2>
+                <p className="menu">
+                    <a href="/"> Home</a> | <a href="/quiz"> Quiz</a> | <a href="/account"> Account</a>
+                </p>
+                <p className="name"> Company Name © 2024</p>
+            </div>
+            <div className="footer-center col-md-4 col-sm-6">
+                <div>
+                    <p>
+                        <span>123 Best Learning</span> Learn, Present
+                    </p>
                 </div>
+                <div>
+                    <p> (+1) 415 240 4280</p>
+                </div>
+                <div>
+                    <p className='email'>
+                        support@lrnr.org
+                    </p>
+                </div>
+            </div>
+            <div className="footer-right col-md-4 col-sm-6">
+                <p className="about">
+                    <span> About the company</span> Welcome to LRNR, your dynamic platform for creating personalized coding quizzes! At LRNR, we empower users to effortlessly generate custom quizzes tailored to their preferences. Dive into the world of coding languages with LRNR. It is your go-to destination for fun and effective learning. Unleash your coding prowess and make learning an adventure with LRNR!
+                </p>
             </div>
         </footer>
     );
 }
 
 export default Footer;
+
+
+
+
+
+
+
+
