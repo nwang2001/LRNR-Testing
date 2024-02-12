974739b737854ff043815f403e3f2e3d211cffc4
Nikki Wang <nikkiw011201@gmail.com>
2024-02-12 12:46:34 -0500

    added responsiveness to account, home, and result page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                         Your guided path to programming enlightenment. Become proficient in{' '}
                         <Typewriter
                             options={{
-                                strings: ['JavaScript', 'Python', 'Java', 'C#', 'Ruby'],
+                                strings: ['JavaScript.', 'Python.', 'Java.', 'C#.', 'Ruby.'],
                                 autoStart: true,
                                 loop: true,
                             }}
@@ -31,29 +31,26 @@ const Home = () => {
             <div className="cards">
                 <div className="flip">
                     <div className="front" src="background-image: url(https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb)">
-                        <h1>Rewards</h1>
+                        <h1 className='gradient-text'>Rewards</h1>
                     </div>
                     <div className="back">
-                        <h2>Rewards</h2>
-                        <p>Our app is designed to be both challenging and rewarding, so you can learn new concepts while enjoying the process. With our personalized quiz app, you can track your progress, compete with your peers, and discover new areas of expertise. The journey of a thousand lines of code begins with a single keystroke</p>
+                        <p>Our app is designed to be both challenging and rewarding, so you can learn new concepts while enjoying the process. With our personalized quiz app, you can track your progress, compete with your peers, and discover new areas of expertise. The journey of a thousand lines of code begins with a single keystroke.</p>
                     </div>
                 </div>
                 <div className="flip">
                     <div className="front" src="background-image: url(https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb)">
-                        <h1>Personal SME</h1>
+                        <h1 className='gradient-text'>Personal SME</h1>
                     </div>
                     <div className="back">
-                        <h2>Personal SME</h2>
-                        <p>Welcome to the path of knowledge. Our app is like having a personal subject matter expert at your side, guiding you on your journey towards wisdom</p>
+                        <p className='center-cards'>Welcome to the path of knowledge. Our app is like having a personal subject matter expert at your side, guiding you on your journey towards wisdom.</p>
                     </div>
                 </div>
                 <div className="flip">
                     <div className="front" src="background-image: url(https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb)">
-                        <h1>Personalized Quizzes</h1>
+                        <h1 className='gradient-text'>Personalized Quizzes</h1>
                     </div>
                     <div className="back">
-                        <h2>Personalized Quizzes</h2>
-                        <p>Good tools make application development quicker and easier to maintain than if you did everything by hand.</p>
+                        <p className='center-cards'>Good tools make application development quicker and easier to maintain than if you did everything by hand.</p>
                     </div>
                 </div>
             </div>
